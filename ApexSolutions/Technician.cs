ad8267aa59ff73ae0e5f6129280557dadcb8d0fb
Justin-Nagel <justinnagel8000@gmail.com>
2024-10-03 11:11:31 +0200

    Test

---
+++ ApexSolutions/Technician.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexSolutions
+{
+    public class Technician
+    {
+        // Fields
+        private int technicianID;
+        private string name;
+        private string skills;
+        private bool availabilityStatus;
+        private List<int> assignedRequests;
+
+        // Properties
+        public int TechnicianID { get => technicianID; set => technicianID = value; }
+        public string Name { get => name; set => name = value; }
+        public string Skills { get => skills; set => skills = value; }
+        public bool AvailabilityStatus { get => availabilityStatus; set => availabilityStatus = value; }
+        public List<int> AssignedRequests { get => assignedRequests; }
+
+        // Constructor
+        public Technician(int technicianID, string name, string skills)
+        {
+            this.technicianID = technicianID;
+            this.name = name;
+            this.skills = skills;
+            this.availabilityStatus = true; // default to available
+            this.assignedRequests = new List<int>();
+        }
+
+        // Methods
+        public void UpdateAvailability(bool isAvailable)
+        {
+            this.availabilityStatus = isAvailable;
+        }
+
+        public void ReceiveJob(int requestID)
+        {
+            assignedRequests.Add(requestID);
+        }
+    }
+}
