b45dbe17f3e72feac88e8ad4db23b8c242507ef6
JJ De Kock <68385686+Rapterian@users.noreply.github.com>
2024-10-06 17:07:16 +0200

    Added UI project to solution including views and controller that will be needed

---
+++ ApexUI/Controllers/ServiceRequestController.cs
@@ -0,0 +1,46 @@
+﻿using ApexSolutions.DTOs;
+using ApexSolutions.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApexUI.Controllers
+{
+    public class ServiceRequestController : Controller
+    {
+        private readonly IServiceRequestRepository _serviceRequestService;
+
+        public ServiceRequestController(IServiceRequestRepository serviceRequestService)
+        {
+            _serviceRequestService = serviceRequestService;
+        }
+
+        public IActionResult Index()
+        {
+            var requests = _serviceRequestService.GetAllAsync();
+            return View(requests);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ServiceRequestDTO dto)
+        {
+            if (ModelState.IsValid)
+            {
+                _serviceRequestService.CreateServiceRequest(dto);
+                return RedirectToAction("Index");
+            }
+            return View(dto);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var request = _serviceRequestService.GetServiceRequestById(id);
+            if (request == null) return NotFound();
+            return View(request);
+        }
+    }
+
+}
