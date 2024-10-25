22dd17de0d87342125563c34413a1bd8881861eb
JJ De Kock <68385686+Rapterian@users.noreply.github.com>
2024-10-25 20:50:23 +0200

    API implementaition

--- ApexSolutions/Models/Technician.cs
+++ ApexSolutions/Models/Technician.cs
@@ -14,6 +14,7 @@ namespace ApexSolutions.Models
         private string skills;
         private bool availabilityStatus;
         private List<int> assignedRequests;
+        private string contactNumber;
 
         // Properties
         public int TechnicianID { get => technicianID; set => technicianID = value; }
@@ -21,15 +22,17 @@ namespace ApexSolutions.Models
         public string Skills { get => skills; set => skills = value; }
         public bool AvailabilityStatus { get => availabilityStatus; set => availabilityStatus = value; }
         public List<int> AssignedRequests { get => assignedRequests; }
+        public string ContactNumber { get => contactNumber; set => contactNumber = value; }
 
         // Constructor
-        public Technician(int technicianID, string name, string skills)
+        public Technician(int technicianID, string name, string skills, string contactNumber)
         {
             this.technicianID = technicianID;
             this.name = name;
             this.skills = skills;
             availabilityStatus = true; // default to available
             assignedRequests = new List<int>();
+            this.contactNumber = contactNumber;
         }
 
         // Methods
